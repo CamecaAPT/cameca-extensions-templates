82a5f17aba6276887e676cd25b90e884d0726c10
Jim Payne <jim.payne@ametek.com>
2022-10-30 21:40:51 -0500

    Add standard custom analysis template

---
+++ Cameca.Extensions.Templates/StandardCustomAnalysis/ProjectNamePlaceholder/SanitizedNamePlaceholderView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectNamePlaceholder;
+
+/// <summary>
+/// Interaction logic for SanitizedNamePlaceholderView.xaml
+/// </summary>
+internal partial class SanitizedNamePlaceholderView
+{
+    public SanitizedNamePlaceholderView()
+    {
+        InitializeComponent();
+    }
+}
