c9e83fb48c6e195f911d7ecad94c9e1c3100b4b5
Jim Payne <jim.payne@ametek.com>
2024-02-29 22:40:43 -0600

    Fix entry point Python file

--- Cameca.Extensions.Templates/PythonCustomAnalysis/ProjectNamePlaceholder/PythonModules/SanitizedNamePlaceholderModule.py
+++ Cameca.Extensions.Templates/PythonCustomAnalysis/ProjectNamePlaceholder/PythonModules/SanitizedNamePlaceholderModule.py
@@ -1,8 +1,7 @@
 import typing
-import numpy as np
 import pyapsuite
 
-def main(context: pyapsuite.APSuiteContext) -> typing.Optional[np.ndarray[np.uint64]]:
+def main(context: pyapsuite.APSuiteContext) -> typing.Optional[typing.Iterable[int]]:
 	# Return a collection of ion indices to filter to that selection in AP Suite
 	# For best performance, return a np.ndarray(dtype=np.uint64)
 	return None
