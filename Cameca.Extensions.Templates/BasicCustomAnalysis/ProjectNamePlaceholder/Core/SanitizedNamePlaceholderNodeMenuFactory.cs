e0a6c10c22ef48d01b5f54e3b19bdd5733d4e6eb
Jim Payne <jim.payne@ametek.com>
2025-02-27 23:26:07 -0600

    Add basic analysis template

---
+++ Cameca.Extensions.Templates/BasicCustomAnalysis/ProjectNamePlaceholder/Core/SanitizedNamePlaceholderNodeMenuFactory.cs
@@ -0,0 +1,17 @@
+﻿using Cameca.CustomAnalysis.Interface;
+using Cameca.CustomAnalysis.Utilities;
+using Prism.Events;
+
+namespace ProjectNamePlaceholder;
+
+internal class SanitizedNamePlaceholderMenuFactory : AnalysisMenuFactoryBase
+{
+    public SanitizedNamePlaceholderMenuFactory(IEventAggregator eventAggregator)
+        : base(eventAggregator)
+    {
+    }
+
+    protected override INodeDisplayInfo DisplayInfo => SanitizedNamePlaceholder.DisplayInfo;
+    protected override string NodeUniqueId => SanitizedNamePlaceholder.UniqueId;
+    public override AnalysisMenuLocation Location { get; } = AnalysisMenuLocation.Analysis;
+}
---
+++ Cameca.Extensions.Templates/BasicCustomAnalysis/ProjectNamePlaceholder/SanitizedNamePlaceholder.cs
@@ -0,0 +1,24 @@
+﻿using Cameca.CustomAnalysis.Interface;
+using Cameca.CustomAnalysis.Utilities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjectNamePlaceholder;
+
+[DefaultView(SanitizedNamePlaceholderViewModel.UniqueId, typeof(SanitizedNamePlaceholderViewModel))]
+internal partial class SanitizedNamePlaceholder : BasicCustomAnalysisBase<SanitizedNamePlaceholderProperties>
+{
+    public const string UniqueId = "ProjectNamePlaceholder.SanitizedNamePlaceholder";
+    
+    public static INodeDisplayInfo DisplayInfo { get; } = new NodeDisplayInfo("DisplayNamePlaceholder");
+
+    public SanitizedNamePlaceholder(IStandardAnalysisFilterNodeBaseServices services, ResourceFactory resourceFactory)
+        : base(services, resourceFactory)
+    {
+    }
+
+    protected override Task<bool> Update(CancellationToken cancellationToken)
+    {
+        return base.Update(cancellationToken);
+    }
+}
